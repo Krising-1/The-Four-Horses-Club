6cb0f372270a0d2a70aa1f698942c231d84058b5
Semon <byuvi@mail.ru>
2024-06-16 20:06:06 +0300

    финальный проверенный вариант

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import { deleteClass } from "./nojs.js";
+import { addButtonsPlane } from "./plane-swiper.js"
+import { addButtonsMembers } from "./members-swiper.js"
+
+
+deleteClass();
+addButtonsPlane();
+addButtonsMembers();
